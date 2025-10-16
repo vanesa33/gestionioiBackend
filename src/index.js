4fbfb955d915a773d9936e477e1584fb88b8302b
vanesSol <tucorreo@example.com>
2025-10-16 02:55:00 -0300

    Fix cors for Vercel frontend

--- src/index.js
+++ src/index.js
@@ -17,8 +17,9 @@ const app = express();
 
 
 app.use(cookieParser());
+
 app.use(cors({
-   origin: 'http://localhost:5173',
+   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
    credentials: true
 }));
 
