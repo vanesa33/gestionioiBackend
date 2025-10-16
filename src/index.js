3454bfbc01121a6d6e94280c574e3e892528dc11
Vanesa <57409926+vanesa33@users.noreply.github.com>
2025-10-16 18:25:42 -0300

    Fix CORS middleware logic for allowed originsif

--- src/index.js
+++ src/index.js
@@ -28,18 +28,19 @@ const allowedOrigins = [ 'http://localhost:5173',
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  if (!origin || allowedOrigins.includes(origin)) {
+   
+  if (origin && allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-    if (req.method === "OPTIONS") return res.sendStatus(204);
-    return next();
-  } else {
-    return res.status(403).send("CORS blocked");
-  }
+   
+  } 
 
-  credentials: true;
+   if (req.method === "OPTIONS") { 
+      return res.sendStatus(204);
+   }  
+ next();
 });
 
 
