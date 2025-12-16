8c2465a2282e14f8e1c262f362514201c75f8d98
Vanesa <57409926+vanesa33@users.noreply.github.com>
2025-12-16 15:52:21 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,8 +1,27 @@
 const { Pool } = require("pg");
+const { db } = require("./config.js");
+require("dotenv").config({ path: './.env' });
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
+let pool;
 
-module.exports = pool; // 👈 IMPORTANTE
+if (process.env.DATABASE_URL) {
+  // 🔹 Conexión en Render o entorno remoto
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+  console.log("✅ Conectado a la base de datos en Render");
+  
+} else {
+  // 🔹 Conexión local (usando tu archivo config.js)
+  pool = new Pool({
+    user: db.user,
+    password: db.password,
+    host: db.host,
+    port: db.PORT,
+    database: db.database,
+  });
+  console.log("✅ Conectado a la base de datos local");
+}
+
+module.exports = { pool };
