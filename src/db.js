009a1e473b996d815175aade06e01879f83e7134
Vanesa <57409926+vanesa33@users.noreply.github.com>
2025-12-16 15:49:50 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,9 +3,6 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  max: 5,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 5000,
 });
 
-module.exports = pool;
+module.exports = pool; // 👈 IMPORTANTE
