ce08411707981ad08d9093fc42ad0badeb48ff40
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-04-08 16:08:03 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,22 +26,23 @@ const allowedOrigins = [ 'http://localhost:5173',
 
 ];
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-   
-  if (origin && allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-   
-  } 
+app.use(cors({
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "https://frontioi.netlify.app",
+      "https://gestionioi-front.vercel.app",
+      "https://gestionioi-front-git-main-vanesols-projects.vercel.app"
+    ];
 
-   if (req.method === "OPTIONS") { 
-      return res.sendStatus(204);
-   }  
- next();
-});
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+  credentials: true
+}));
 
 
 
